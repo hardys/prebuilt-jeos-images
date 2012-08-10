45b8806aa70ad2958c64f2a6a131bec1304d16ac
Steven Dake <sdake@redhat.com>
2012-08-10 10:24:17 -0700

    Add an uploading script for this repo
    Signed-off-by: Steven Dake <sdake@redhat.com>

---
+++ upload.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import os
+from pygithub3 import Github
+from pygithub3.services.repos import Downloads
+
+user_name = 'sdake'
+user_password = 'SETME'
+repo_user_name = 'heat-api'
+repo_name = 'prebuilt-jeos-images'
+
+#              'F17-i386-cfntools-joes')
+jeos_names = ('F16-x86_64-cfntools-jeos',
+              'F16-i386-cfntools-jeos',
+              'F17-x86_64-cfntools-jeos')
+
+gh = Github(login=user_name, password=user_password)
+
+upload_dicts=[]
+for jeos_name in jeos_names:
+    try: 
+        filename = '/var/lib/libvirt/images/%s.qcow2' % jeos_name
+        statinfo = os.stat(filename)
+        size = statinfo.st_size
+        upload_dicts.append(dict(name='%s.qcow2' % jeos_name, size='%d' % size))
+    except:
+        print "Can't find file '%s' to upload to github." % filename
+
+print "Uploading files to github user '%s' repo '%s'" % (repo_user_name, repo_name)
+for upload_dict in upload_dicts:
+    print 'Uploading file %s - size %d mb' % (upload_dict["name"],
+           int(upload_dict["size"]) / (1024*1024))
+    download = gh.repos.downloads.create(upload_dict, user=repo_user_name,
+                                          repo=repo_name)
+    download.upload(filename)
