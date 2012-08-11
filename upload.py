039443db290c9bbc987f70c47184161c47a5c5ab
Steven Dake <sdake@redhat.com>
2012-08-10 17:30:17 -0700

    Updated upload.py to delete existing uploads if present
    Signed-off-by: Steven Dake <sdake@redhat.com>

--- upload.py
+++ upload.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python
-
 import os
 from pygithub3 import Github
 from pygithub3.services.repos import Downloads
 
-user_name = 'sdake'
-user_password = 'SETME'
 repo_user_name = 'heat-api'
 repo_name = 'prebuilt-jeos-images'
 
-#              'F17-i386-cfntools-joes')
 jeos_names = ('F16-x86_64-cfntools-jeos',
               'F16-i386-cfntools-jeos',
-              'F17-x86_64-cfntools-jeos')
+              'F17-x86_64-cfntools-jeos',
+              'F17-i386-cfntools-jeos',
+              'U10-x86_64-cfntools-jeos')
 
-gh = Github(login=user_name, password=user_password)
+gh = Github(login='PUT_YOUR_GITHUB_NAME_HERE', password='PUT_YOUR_PASSWORD_HERE')
 
 upload_dicts=[]
 for jeos_name in jeos_names:
@@ -22,14 +20,24 @@ for jeos_name in jeos_names:
         filename = '/var/lib/libvirt/images/%s.qcow2' % jeos_name
         statinfo = os.stat(filename)
         size = statinfo.st_size
-        upload_dicts.append(dict(name='%s.qcow2' % jeos_name, size='%d' % size))
+        upload_dicts.append(dict(name='%s.qcow2' % jeos_name, size='%d' % size,
+                            filename=filename))
     except:
         print "Can't find file '%s' to upload to github." % filename
 
 print "Uploading files to github user '%s' repo '%s'" % (repo_user_name, repo_name)
 for upload_dict in upload_dicts:
+    # Delete an existing upload
+    files = gh.repos.downloads.list(user=repo_user_name, repo=repo_name).all()
+    for file in files:
+        if file.name == upload_dict["name"]:
+            print 'Deleting existing upload %s' % upload_dict["name"]
+            gh.repos.downloads.delete(id=file.id, user=repo_user_name,
+                repo=repo_name)
+            break
+
     print 'Uploading file %s - size %d mb' % (upload_dict["name"],
            int(upload_dict["size"]) / (1024*1024))
     download = gh.repos.downloads.create(upload_dict, user=repo_user_name,
                                           repo=repo_name)
-    download.upload(filename)
+    download.upload(upload_dict["filename"])
